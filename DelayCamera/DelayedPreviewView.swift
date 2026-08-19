8584b5dc1d5030567ffc0654ac885713f1fdbe44
rose8050 <heeho805@gmail.com>
2026-08-19 15:09:34 +0900

    Update DelayedPreviewView.swift

--- DelayCamera/DelayedPreviewView.swift
+++ DelayCamera/DelayedPreviewView.swift
@@ -31,26 +31,3 @@ final class PreviewContainerView: UIView {
         CATransaction.commit()
     }
 }
-
-struct GridOverlayView: View {
-    var body: some View {
-        GeometryReader { geometry in
-            Path { path in
-                let width = geometry.size.width
-                let height = geometry.size.height
-                for i in 1...2 {
-                    let x = width * CGFloat(i) / 3
-                    path.move(to: CGPoint(x: x, y: 0))
-                    path.addLine(to: CGPoint(x: x, y: height))
-
-                    let y = height * CGFloat(i) / 3
-                    path.move(to: CGPoint(x: 0, y: y))
-                    path.addLine(to: CGPoint(x: width, y: y))
-                }
-            }
-            .stroke(Color.white.opacity(0.45), lineWidth: 0.75)
-            .shadow(color: .black.opacity(0.35), radius: 1)
-        }
-        .allowsHitTesting(false)
-    }
-}
